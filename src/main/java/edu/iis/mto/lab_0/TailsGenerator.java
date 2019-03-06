daa8efecab3678d913a168588857e9db4c3ab705
Maciej.Kopka <209360@edu.p.lodz.pl>
2019-03-06 18:47:03 +0100

    tails return first full string at first time

--- src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
+++ src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
@@ -1,14 +1,18 @@
 package edu.iis.mto.lab_0;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class TailsGenerator {
 
 	public List<String> tails(String value) {
-		if (value == null)
-			return new ArrayList<>();
-		return new ArrayList<>(value.length());
+		if (value == null) {
+			return Collections.EMPTY_LIST;
+		}
+		ArrayList<String> list = new ArrayList<>(value.length());
+		list.add(0, value);
+		return list;
 	}
 
 }
